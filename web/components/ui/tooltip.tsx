657c015a9ac26e0fa5fdb64072f344d9aba2b5c4
enermation <admin@enermation.co>
2026-04-30 01:55:55 +0500

    fix(tooltip): disable nativeButton to prevent nested button issue

--- web/components/ui/tooltip.tsx
+++ web/components/ui/tooltip.tsx
@@ -13,7 +13,7 @@ function Tooltip({ ...props }: TooltipPrimitive.Root.Props) {
 }
 
 function TooltipTrigger(props: TooltipPrimitive.Trigger.Props) {
-  return <TooltipPrimitive.Trigger data-slot="tooltip-trigger" {...props} />
+  return <TooltipPrimitive.Trigger data-slot="tooltip-trigger" nativeButton={false} {...props} />
 }
 
 function TooltipContent({
