dbd0685d7e39a51b3aefe106efbb9e9725f9d1eb
imossaidqadri <imossaidquadri@gmail.com>
2026-05-02 00:43:22 +0500

    style: reformat dynamic import in ChatWidgetMount
    Collapse multi-line dynamic() call into single line for consistency

--- web/components/chat-widget-mount.tsx
+++ web/components/chat-widget-mount.tsx
@@ -2,10 +2,9 @@
 
 import dynamic from 'next/dynamic'
 
-const ChatWidget = dynamic(
-  () => import('@/components/rag/chat-widget').then(m => m.ChatWidget),
-  { ssr: false }
-)
+const ChatWidget = dynamic(() => import('@/components/rag/chat-widget').then(m => m.ChatWidget), {
+  ssr: false,
+})
 
 export function ChatWidgetMount() {
   return <ChatWidget />
